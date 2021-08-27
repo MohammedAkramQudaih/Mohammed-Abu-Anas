1bb695be1b0f43e6a8343759d0127a7188ec606c
MohammedAkramQudaih <75170603+MohammedAkramQudaih@users.noreply.github.com>
2021-08-27 10:51:34 +0300

    Add files via upload

---
+++ js/fainal.js
@@ -0,0 +1,67 @@
+function nn(){
+
+    alert("تم اشتراكك بالبريد الالكتروني بنجاح")
+}
+function mm(){
+   
+    if(confirm("هل تريد ارسال رسالتك")){
+        alert("تم ارسال رسالتك بنجاح")
+    }else
+        alert("لم يتم ارسال رسالتك")
+
+
+
+
+
+
+
+
+
+
+}
+function G() {
+    document.getElementById("h4").setAttribute("style","display:none;");
+    document.getElementById("h5").setAttribute("style","display:none;");
+    document.getElementById("h6").setAttribute("style","display:none;");
+    document.getElementById("h7").setAttribute("style","display:none;");
+    document.getElementById("h8").setAttribute("style","display:none;");
+    document.getElementById("h9").setAttribute("style","display:none;");
+    document.getElementById("h1").setAttribute("style","display:block;");
+    document.getElementById("h2").setAttribute("style","display:block;");
+    document.getElementById("h3").setAttribute("style","display:block;");
+    document.getElementById("btn2").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn3").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn1").setAttribute("style","border-bottom-color: #821517;border-width: 5px;color: #821517;");
+}
+
+function G1() {
+    document.getElementById("h1").setAttribute("style","display:none;");
+    document.getElementById("h2").setAttribute("style","display:none;");
+    document.getElementById("h3").setAttribute("style","display:none;");
+    document.getElementById("h7").setAttribute("style","display:none;");
+    document.getElementById("h8").setAttribute("style","display:none;");
+    document.getElementById("h9").setAttribute("style","display:none;");
+    document.getElementById("h4").setAttribute("style","display:block;");
+    document.getElementById("h5").setAttribute("style","display:block;");
+    document.getElementById("h6").setAttribute("style","display:block;");
+    document.getElementById("btn1").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn3").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn2").setAttribute("style","border-bottom-color: #821517;border-width: 5px;color: #821517;");
+
+}
+
+function G2() {
+    document.getElementById("h1").setAttribute("style","display:none;");
+    document.getElementById("h2").setAttribute("style","display:none;");
+    document.getElementById("h3").setAttribute("style","display:none;");
+    document.getElementById("h4").setAttribute("style","display:none;");
+    document.getElementById("h5").setAttribute("style","display:none;");
+    document.getElementById("h6").setAttribute("style","display:none;");
+    document.getElementById("h7").setAttribute("style","display:block;");
+    document.getElementById("h8").setAttribute("style","display:block;");
+    document.getElementById("h9").setAttribute("style","display:block;");
+    document.getElementById("btn1").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn2").setAttribute("style","border-bottom-color: black; border-width: 3px; color:black;");
+    document.getElementById("btn3").setAttribute("style","border-bottom-color: #821517;border-width: 5px;color: #821517;");
+
+}
